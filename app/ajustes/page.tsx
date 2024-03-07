5ad52458ca2e767bfe3e47ccd7b95d0b29fe2c15
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-03-07 06:57:16 -0300

    criando paginas e melhorando componente titulo

---
+++ app/ajustes/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/template/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout
+      titulo="Ajustes e Configurações"
+      subtitulo="Personalize o sistema aqui."
+    >
+      <h3>conteúdo!!!</h3>
+    </Layout>
+  );
+}

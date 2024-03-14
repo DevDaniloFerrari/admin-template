083c2d007a0ad06186503e6c69114cb52b55f8ae
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-03-14 06:37:55 -0300

    estilizando botao de alternar tema

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -66,14 +66,14 @@ export const IconeSair = (
   </svg>
 );
 
-export const IconeSol = (
+export const IconeSol = (tamanho = 6) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className={`h-${tamanho} w-${tamanho}`}
   >
     <path
       strokeLinecap="round"
@@ -83,14 +83,14 @@ export const IconeSol = (
   </svg>
 );
 
-export const IconeLua = (
+export const IconeLua = (tamanho = 6) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className={`h-${tamanho} w-${tamanho}`}
   >
     <path
       strokeLinecap="round"

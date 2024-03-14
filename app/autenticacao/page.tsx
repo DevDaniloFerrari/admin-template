e091f1b49551ebe9c769eb04e032177d9fe62ccf
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-03-14 07:24:15 -0300

    melhorando responsividade e alternancia entre modos

--- app/autenticacao/page.tsx
+++ app/autenticacao/page.tsx
@@ -13,11 +13,18 @@ export default function Autenticacao() {
   }
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <div className="w-1/2">
+    <div className="flex h-screen items-center justify-center">
+      <div className="hidden md:block md:w-1/2 lg:w-2/3">
+        <img
+          src="https://source.unsplash.com/random"
+          alt="Imagem da Tela de Autenticação"
+          className="h-screen w-full object-cover"
+        />
+      </div>
+      <div className="m-10 w-full md:w-1/2 lg:w-1/3">
         <h1
           className={`
-        text-xl font-bold mb-5
+        text-3xl font-bold mb-5
       `}
         >
           {modo === "login"
@@ -40,7 +47,7 @@ export default function Autenticacao() {
         />
         <AuthInput
           tipo="password"
-          label="Senha"
+          label="Confirmar Senha"
           valor={senha}
           valorMudou={setSenha}
           obrigatorio
@@ -67,6 +74,28 @@ export default function Autenticacao() {
         >
           Entrar com Google
         </button>
+
+        {modo === "login" ? (
+          <p className="mt-8">
+            Novo por aqui?
+            <a
+              onClick={() => setModo("cadastro")}
+              className={`
+                    text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                `}
+            > Crie uma conta gratuitamente</a>
+          </p>
+        ) : (
+          <p className="mt-8">
+            Já faz parte da nossa comunidade?
+            <a
+              onClick={() => setModo("login")}
+              className={`
+                    text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                `}
+            > Entre com as suas credenciais</a>
+          </p>
+        )}
       </div>
     </div>
   );
